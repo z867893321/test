fde2a420579f288ca504b9b7d83ae98b6db2e849
z867893321 <zmf19941222@163.com>
2017-04-14 13:52:32 +0800

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,28 @@
+sudo cd /opt
+sudo wget https://download.docker.com/linux/ubuntu/dists/trusty/pool/stable/amd64/docker-ce_17.03.0~ce-0~ubuntu-trusty_amd64.deb
+sudo dpkg -i docker-ce_17.03.0-ce-0~ubuntu-trusty_amd64.deb
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://ae6kri8o.mirror.aliyuncs.com"]
+}
+EOF
+
+
+#sudo docker swarm join-token worker|awk 'NR>2{print$0}' >>/opt/worker.sh
+#sudo docker swarm join-token manager|awk 'NR>2{print$0}' >>/opt/manager.sh
+#sudo apt-get install nfs-kernel-server -y
+#sudo tee /etc/exports <<-'EOF'
+#/opt/ *(rw,sync,no_root_squash,no_subtree_check)
+#EOF
+#sudo rpc.mountd 
+#sudo service nfs-kernel-server restart 
+
+
+
+
+########################
+#sudo apt-get install nfs-common
+#mount -t nfs ucp01/opt /opt
+#bash /opt/workt.sh
+###########
+#bash /opt/manager.sh
